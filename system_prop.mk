5e8165ecfdd3dbc6e7ca8d5df88535f03197bd67
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-11-07 14:20:36 +0000

    msm8916-common: Set ro.vndk.version to current
    Change-Id: Icd3f5db2777574fc649d55cfc4991b4f39e41429

--- system_prop.mk
+++ system_prop.mk
@@ -62,7 +62,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # System
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.kernel.ebpf.supported=false \
-    persist.sys.binary_xml=false
+    persist.sys.binary_xml=false \
+    ro.vndk.version=current
 
 # Wi-Fi
 PRODUCT_PROPERTY_OVERRIDES += \
